332492ac40d8214ba5df33100528f450bb126216
bluemyria <mlekkou@gmail.com>
2018-11-06 10:53:57 +0200

    ScheduledCard added for DayView

--- packages/examples/src/app/sortable/kanban/day.component.ts
+++ packages/examples/src/app/sortable/kanban/day.component.ts
@@ -3,7 +3,7 @@ import { SkyhookDndService } from "@angular-skyhook/core";
 import { ItemTypes } from "./item-types";
 import { DraggedItem } from "@angular-skyhook/sortable";
 import { Output } from "@angular/core";
-import { Card } from './card';
+import { Card, ScheduledCard } from './card';
 
 @Component({
     selector: 'kanban-day',
@@ -16,6 +16,11 @@ import { Card } from './card';
         <div>
             <i class="fas fa-calendar"></i>
             <span>Drop here to schedule</span>
+
+            <div class="printout-elem" *ngFor="let scheduledCard of scheduledCards">
+                <h4>{{scheduledCard.title}}</h4>
+            </div>
+
         </div>
         <!--div class="space" [ngStyle]="getStyle(c.isOver, c.item)"></div-->
     </div>
@@ -36,6 +41,20 @@ import { Card } from './card';
         //transform-origin: 100% 100%;
         //transition: transform 50ms ease-out;
     }
+    .printout-elem {
+        position: relative;
+        margin: 2px;
+        padding: 2px;
+        font-weight: 700;
+        text-shadow: 1px 1px rgba(255,255,255,0.2);
+        border-radius: 2px;
+        border: 1px dotted #000;
+        text-align: left;
+        //height: 300px;
+        //width: 250px;
+        //transform-origin: 100% 100%;
+        //transition: transform 50ms ease-out;
+    }
     .space {
         height: 0;
         width: 0;
@@ -49,13 +68,27 @@ import { Card } from './card';
     `]
 })
 export class DayComponent {
+    scheduledCard: ScheduledCard;
+    scheduledCards: ScheduledCard[] = [];
+    myDraggedItem: DraggedItem<Card>;
+
     @Output() dropped = new EventEmitter<DraggedItem<Card>>();
     target = this.dnd.dropTarget<DraggedItem<Card>>(ItemTypes.CARD, {
         canDrop: monitor => {
             return monitor.getItem().isInternal;
         },
         drop: monitor => {
+            console.log(monitor.getItem());
+            console.log(monitor.getClientOffset());
+            console.log(monitor.getInitialClientOffset());
             this.dropped.emit(monitor.getItem());
+
+            this.myDraggedItem = monitor.getItem();
+            this.scheduledCard = {
+                id: monitor.getItem().data.id,
+                title: monitor.getItem().data.title
+            };
+            this.scheduledCards.push(this.scheduledCard);
         }
     });
     collect$ = this.target.listen(m => ({
